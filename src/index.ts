338ec0d064ce49a14fc4fd383dba867f102d9133
Ash <ashnamuh@gmail.com>
2020-06-04 15:48:39 +0900

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
-import { render } from './core'
+import { Component, render } from './core'
+import request from './lib/request'
 
 import './styles/index.css'
 
-const Root: Component = () => {
-  return `hello${1}`
+class Child implements Component {
+  constructor() {
+    console.log(11)
+  }
+  render() {
+    console.log('child render')
+    return '<span>hello</span>'
+  }
 }
 
-render('root', Root)
+interface State {
+  count: number;
+}
+
+class Root implements Component { // eslint-disable-line
+  state: State = {
+    count: 0
+  }
+  constructor() {
+    this.mounted()
+  }
+  setState(newState: State) {
+    this.state = { ...newState }
+  }
+  async mounted () {
+    const res = await fetch('https://conduit.productionready.io/api/articles')
+    console.log(res)
+  }
+  click() {
+    console.log('clicked')
+  }
+
+  render() {
+    return `<p onClick="this.click()">${this.state.count}${render(Child)}</p>`
+  }
+}
+
+(async () => {
+  console.log(1)
+  const r = await request('https://conduit.productionready.io/api/articles')
+  console.log(2)
+  console.log(r)
+})()
